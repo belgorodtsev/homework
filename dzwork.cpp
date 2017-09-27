0b89d04228545668211490f7cbd4f62aeba9d34c
belgorodtsev <32188418+belgorodtsev@users.noreply.github.com>
2017-09-27 15:20:33 +0300

    Update dzwork.cpp

--- dzwork.cpp
+++ dzwork.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 
 int main()
-{	//Вариант2
-	//5
+{	//Вариант2//5
 	std::cout << " Enter a three-digit number " << std::endl;
 	int number = 0;
 	std::cin >> number;
@@ -20,7 +19,7 @@ int main()
 	int number1 = 0;
 	if (n > 9 && n < 990)
 	{
-		number1 = (n / 10) % 10 * 100 + n % 10 + (n / 100) % 10 * 10;
+	number1 = (n / 10) % 10 * 100 + n % 10 + (n / 100) % 10 * 10;
 	}
 	std::cout << " Number = " << number1 << std::endl;
 	//
