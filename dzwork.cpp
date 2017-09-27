39a1903b39adf3552d229f2b303326ff872a47af
belgorodtsev <32188418+belgorodtsev@users.noreply.github.com>
2017-09-27 17:41:59 +0300

    Update dzwork.cpp

--- dzwork.cpp
+++ dzwork.cpp
@@ -6,8 +6,7 @@ int main()
     std::cout << " Enter a three-digit number " << std::endl;
     int number = 0;
     std::cin >> number;
-    int b = 0;
-    b = number / 100;
+    int b = number / 100;
     number = number % 100;
     number = number * 10 + b;
     std::cout << number << std::endl;
